f0cff946aace4a3211a104b47c914016c4c71813
jounghun-kwak <92780221+jounghun-kwak@users.noreply.github.com>
2022-09-29 10:33:28 +0900

    Create pc_math.c
    math fuction test

---
+++ math/pc_math.c
@@ -0,0 +1,35 @@
+#include "HT66F2390.h"
+#include <math.h>
+
+double g_y, g_y, g_y;
+const double PI = 3.1415926;
+void main()
+{
+	int x = 90;
+	int z = 0;
+	double e;
+	g_y = sin(x * PI / 180);//1
+	g_y = cos(x * PI / 180);
+	g_y = tan(x * PI / 180);
+	g_y = atan2(1.0, 1.0);
+	g_y = ceil(x * PI / 180);
+	g_y = cos(x * PI / 180);
+	g_y = exp(x * PI / 180);
+	g_y = fabs(x * PI / 180);
+	g_y = floor(x * PI / 180);
+	g_y = frexp(x * PI / 180, &z);
+	g_y = ldexp(x * PI / 180, z);
+	g_y = log(x * PI / 180);
+	g_y = log10(x * PI / 180);
+	g_y = modf(1.2345, &e);
+	g_y = pow(2.0, 3.0);//8
+	g_y = sin(x * PI / 180);
+	g_y = sqrt(25.0);//5
+	g_y = tan(x * PI / 180);
+	g_y = sinh(x * PI / 180);
+	g_y = cosh(x * PI / 180);
+	g_y = tanh(x * PI / 180);
+	while(1);
+
+
+}
